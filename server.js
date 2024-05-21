1cdb76efe2dd058a5432be97f3137815cc207315
mbottino@hms-tech.com <mbottino01@outlook.com>
2024-05-21 00:14:14 -0300

    Adding cors in the project

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
